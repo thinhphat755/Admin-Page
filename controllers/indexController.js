a1d659198973663f3c2c2492283cc8b60177ffde
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2019-12-04 20:12:27 +0700

    thay đổi để hiện câu chào người dùng trong trang chủ

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,11 @@
+const User = require('../models/user');
+
 //render index
 exports.home = (req, res, next) =>{
-    res.render('index');
+    User.find({}, (err) => {
+        if(err) {return next(err);}
+        else{
+            res.render('index', {user:req.user});
+        }
+    });
 }
